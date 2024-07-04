b9082089361f62c51958e127fbe6b00e02a44612
Hiroyuki1313 <munozdiaza@gmail.com>
2024-07-04 11:00:51 -0600

    login android

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,6 +1,7 @@
 
-import { Text,View,TextInput,StyleSheet, Pressable,Platform  } from "react-native";
+import { Text,View,TextInput,StyleSheet, Pressable,Platform,Image  } from "react-native";
 import React, { useState } from "react";
+
 import {LinearGradient} from 'expo-linear-gradient';
 import { createStackNavigator } from "@react-navigation/stack";
 import { Stack } from "expo-router";
@@ -32,17 +33,11 @@ export default function(){
                 }}
                 style={styles.box}>
 
-                    <View style={styles.mainContainer} >
-                        <View style={styles.image}><Text style={styles.imageText}>ImagenLogo</Text></View>
-                        <Text style={styles.title}>Iniciar Sesion</Text>
-                        <View style={styles.inputContainer}>
-                            <TextInput style={styles.inputText} placeholder="Usuario" placeholderTextColor={"gray"} onChangeText={setText} value={text}></TextInput>
-                            <TextInput style={styles.inputText} placeholder="Contraseña" placeholderTextColor={"gray"} onChangeText={setPass} value={pass}></TextInput>
-                            <Pressable><Text style={styles.loginButton}>Iniciar</Text></Pressable>
-                        </View>
-                        <Pressable style={styles.noAccount}>
-                            <Text style={styles.noAccount} >¿No tienes cuenta?</Text>
-                        </Pressable>
+                    <View style={styles.mainContainer}>
+                        <Image source={require('../../assets/images/react-logo.png')} style={styles.image}/>
+                            <View>
+                                <Text style={styles.signinText}>Sign in to your Alarme account</Text>
+                            </View>
                     </View>    
 
                 
@@ -66,12 +61,12 @@ const styles = StyleSheet.create({
         
     },
     mainContainer:{
-        justifyContent: "center",
-        backgroundColor: 'rgba(0,0,0,0.6)',
-
+        justifyContent: "flex-start",
+        backgroundColor: 'rgba(255,255,255, 0.9)',
+        alignItems: 'center',
         width: "94%",
         height: "90%",
-
+        paddingTop: 50,
         alignSelf:"center",
         borderStartColor: 'rgba(0,112,255,0.5)',
         borderTopColor: 'rgba(0,112,255,0.5)',
@@ -102,58 +97,15 @@ const styles = StyleSheet.create({
         backgroundColor: "#0B2447",
         flex: 1,
     },
-    image:{
-
-        //borderWidth: 2,
-        //borderColor: 'red',
-        justifyContent:'center',
-        textAlign:'center',
-        marginBottom: 10,
-        marginTop:10,
-        height:100
-
-    },
-    imageText:{
-        textAlign:'center',
-        justifyContent:'center'
-    },
-    inputContainer:{
-        marginTop: 60,
-
-    },
-    inputText:{
-        borderColor: '#19376D',
-        fontSize:20,
-        borderRadius: 5,
-        borderBottomWidth: 2,
-        justifyContent:'center',
-        textAlign:'center',
-        marginHorizontal: 20,
-        marginVertical: 10,
-        paddingHorizontal: 80   ,
-        paddingVertical: 4,
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        color: "#fff"
-        
-
-    },
-    noAccount:{
-        marginTop: 50,
-        color: '#fff',
-        textAlign:'center',
-        marginBottom:6,
-    },
-    loginButton:{
+    signinText:{
         textAlign: "center",
-        justifyContent: "center",
-        fontSize: 28,
-
-        marginHorizontal: "auto",
-        marginTop: 30,
-        paddingVertical: 5,
-        paddingHorizontal: 30,
-        borderRadius: 5,
-        backgroundColor: '#576CBC'
+        fontSize: 18,
+        marginTop: 1
     },
+    image: {
+        width: 100, 
+        height: 100, 
+        marginBottom: 20
+    }
     
 })
